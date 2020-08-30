5efa640fda87d07e0e2b200396adc5f2453bcc81
Michael Spengler <mycamper@gmx.de>
2020-08-30 17:56:41 +0200

    submodules

---
+++ usage-example-sub-module.ts
@@ -0,0 +1,5 @@
+import { logger } from "./usage-example.ts";
+
+export function doSomething() {
+    logger.info('Greetings from the submodule. I hope you enjoy this fancy logger.')
+}
--- usage-example.ts
+++ usage-example.ts
@@ -1,9 +1,13 @@
 
+import { doSomething } from './usage-example-sub-module.ts'
+
 import { Logger } from 'https://deno.land/x/log/mod.ts'
-const logger = await Logger.getInstance()
+export const logger = await Logger.getInstance()
 
 logger.debug('example debug message')
 logger.info('example info')
 logger.warning('example warning')
 logger.error('example error message')
 logger.critical('example critical message')
+
+doSomething()
