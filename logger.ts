6bb2c987df4d76ccd1f6bf41738273931e27eaca
Michael Spengler <mycamper@gmx.de>
2020-08-30 13:17:56 +0200

    infos are green now :)

--- logger.ts
+++ logger.ts
@@ -1,10 +1,22 @@
 import * as log from "https://deno.land/std/log/mod.ts";
+import { green } from "https://deno.land/std@0.67.0/fmt/colors.ts"
 
 export async function getLogger(minLevelForConsole = 'DEBUG', minLevelForFile = 'WARNING', fileName = "./log.txt"): Promise<any> {
     await log.setup({
         handlers: {
             console: new log.handlers.ConsoleHandler(minLevelForConsole as any, {
-                formatter: "{levelName} {datetime} {msg}"
+                formatter: logRecord => {
+                    let msg = `${logRecord.level} ${logRecord.msg}`;
+
+                    logRecord.args.forEach((arg, index) => {
+                        msg += `, arg${index}: ${arg}`;
+                    });
+
+                    if (logRecord.level === 20) {
+                        return green(msg)
+                    }
+                    return msg;
+                }
             }),
 
             file: new log.handlers.FileHandler(minLevelForFile as any, {
