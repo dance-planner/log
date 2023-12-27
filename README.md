13ef16caa2a6131bb0d8c549c82d4ae3ce0104ee
monique <michael.spengler@fh-hwz.ch>
2023-12-27 13:18:13 +0100

    update and support Freedom Cash

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Logger
+
 Frequently used configuration:  
 Writing warnings + errors into file. Adding date and time. Info Messages are green.
 
 ## Usage Example
 
 ```ts
-
-
 import { Logger } from 'https://deno.land/x/log/mod.ts'
 export const logger = await Logger.getInstance() // import this one in your sub modules
 
@@ -16,23 +15,18 @@ logger.warning('example warning')
 logger.error('example error message')
 logger.critical('example critical message')
 
-
 // warnings + errors are additionally written to ./warnings-errors.txt file 
-
 ```
 
 ## Trigger Usage Example
 
 ```sh
-
 deno run --allow-read --allow-write https://deno.land/x/log/usage-example.ts
-
 ```
 
 ## Advanced Usage
 
 ```ts
-
 import { Logger } from 'https://deno.land/x/log/mod.ts'
 
 const minLevelForConsole = 'DEBUG' // config.minLevelForConsole
@@ -44,20 +38,17 @@ const minLevelForFile = 'WARNING' // config.minLevelForFile
 const fileName = "./warnings-errors.txt"
 
 // import this logger in your sub modules so that you have one logger for your whole process
-export const logger = 
-    await Logger.getInstance(minLevelForConsole, minLevelForFile, fileName)
+export const logger = await Logger.getInstance(minLevelForConsole, minLevelForFile, fileName)
 
 logger.debug('example debug message')
 logger.info('example info')
 logger.warning('example warning')
 logger.error('example error message')
 logger.critical('example critical message')
-
 ```
 
-## Support my Open Source Contributions
-
-If you like my work please consider downloading the brave browser via my
-promotion link: [https://brave.com/fan464](https://brave.com/fan464).
-
-![![](https://brave.com/)](https://brave.com/wp-content/uploads/2019/01/logotype-full-color.svg)
+## Donations
+Thanks to [Freedom Cash](https://FreedomCash.org), we are already free.  
+If you want to donate, you might consider donating to the [otherparty.co.uk](https://www.otherparty.co.uk/donate-crypto-the-other-party) to ensure people do not need to donate to victims but rather donate successfully to problem solvers.   
+  
+![direct-democracy](https://github.com/michael-spengler/sleep/assets/145258627/fe97b7da-62b4-4cf6-9be0-7b03b2f3095a)
--- logger.ts
+++ logger.ts
@@ -1,20 +1,18 @@
 import * as log from "https://deno.land/std/log/mod.ts";
-import { green, gray } from "https://deno.land/std@0.67.0/fmt/colors.ts"
+import { green, gray } from "https://deno.land/std@0.210.0/fmt/colors.ts"
 
 export class Logger {
 
     private static instance: any
 
-    public static async getInstance(minLevelForConsole = 'DEBUG', minLevelForFile = 'WARNING', fileName = "./warnings-errors.txt"){
+    public static async getInstance(minLevelForConsole = 'DEBUG', minLevelForFile = 'WARNING', fileName = "./warnings-errors.txt") {
         if (Logger.instance === undefined) {
             log.info(`preparing a new logger \nminLevelForConsole: ${minLevelForConsole} \nminLevelForFile: ${minLevelForFile} \nfileName: ${fileName}`)
             Logger.instance = await Logger.prepareLogger(minLevelForConsole, minLevelForFile, fileName)
         }
-        
         log.info(`delivering logger \nminLevelForConsole: ${minLevelForConsole} \nminLevelForFile: ${minLevelForFile} \nfileName: ${fileName}`)
         return Logger.instance
     }
-
     private static async prepareLogger(minLevelForConsole: string, minLevelForFile: string, fileName: string): Promise<any> {
         await log.setup({
             handlers: {
@@ -22,11 +20,11 @@ export class Logger {
                     formatter: logRecord => {
                         const dateTime = new Date().toISOString()
                         let msg = `[${logRecord.levelName}]: [${dateTime}] ${logRecord.msg}`;
-    
+
                         logRecord.args.forEach((arg, index) => {
                             msg += `, arg${index}: ${arg}`;
                         });
-    
+
                         if (logRecord.level === 10) {
                             return gray(msg)
                         }
@@ -36,13 +34,12 @@ export class Logger {
                         return msg;
                     }
                 }),
-    
+
                 file: new log.handlers.FileHandler(minLevelForFile as any, {
                     filename: `${Deno.cwd()}/${fileName}`,
                     formatter: "{levelName} {datetime} {msg}",
                 }),
             },
-    
             loggers: {
                 default: {
                     level: "DEBUG",
@@ -50,7 +47,7 @@ export class Logger {
                 },
             },
         });
-    
+
         return log.getLogger();
     }
 }
