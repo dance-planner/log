ad2feda4448767acf99f34c1f58ede4cd0c9ff6b
monique <michael.spengler@fh-hwz.ch>
2023-12-28 20:04:34 +0100

    utilize specific version for std to support stability

--- logger.ts
+++ logger.ts
@@ -1,4 +1,4 @@
-import * as log from "https://deno.land/std/log/mod.ts";
+import * as log from "https://deno.land/std@0.210.0/log/mod.ts";
 import { green, gray } from "https://deno.land/std@0.210.0/fmt/colors.ts"
 
 export class Logger {
