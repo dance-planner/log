cc50e354a901039b53393bc505d768a30d2471ef
monique <michael.spengler@fh-hwz.ch>
2023-12-28 16:37:14 +0100

    improve documentation

--- usage-example.ts
+++ usage-example.ts
@@ -3,7 +3,7 @@ import { Logger } from 'https://deno.land/x/log/mod.ts'
 export const logger = await Logger.getInstance()
 
 logger.debug('example debug message')
-logger.info('example info', true)
+logger.info('example info')
 logger.warning('example warning')
 logger.error('example error message')
 logger.critical('example critical message')
